889704038bdae2e4423759eca59b7a63787216a8
ashish1996 <akhanchandani1996@gmail.com>
2018-04-02 15:24:01 -0400

    fix2

--- server.js
+++ server.js
@@ -60,6 +60,12 @@ const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3002;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+
 // Initialize routes middleware
 // app.use(function(err, req,res,next){
 //   req.db = db;
