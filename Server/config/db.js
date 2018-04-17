b7bee11cc305e91b91ec88e89de9c06c2d92e88b
ashish1996 <akhanchandani1996@gmail.com>
2018-04-17 04:20:42 -0400

    stitch

--- Server/config/db.js
+++
@@ -1,6 +0,0 @@
-// Set the connection string based from the config vars of the production server
-// To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
-
-module.exports = {
-  db: 'mongodb://nxhuman:hubal2018@ds115219.mlab.com:15219/heroku_cj4zfrd2'
-};
