b5ffa3421dc38528392b9254d8317f2309e46a38
isharma <>
2018-05-06 17:10:36 -0400

    adding more save capability

--- src/routes/caseoverview.js
+++ src/routes/caseoverview.js
@@ -9,6 +9,9 @@ const url = 'mongodb://nxhuman:hubal2018@ds115219.mlab.com:15219/heroku_cj4zfrd2
 var db =require('mongodb')(url);
 var mongodb= require('mongodb');
 var MongoClient= mongodb.MongoClient;
+let client;
+let db; 
+
 
 
 router.post('/test', function(req, res, next) {
@@ -82,6 +85,21 @@ router.get('/test', function(req, res, next) {
     });
   });
 ; 
+
+function displayCommentsOnLoad() {
+   clientPromise.then(stitchClient => {
+       client = stitchClient;
+       db = client.service('mongodb', 'mongodb-atlas').db('nxhuman');
+       return client.login().then(displayComments)
+   });
+}
+
+function displayComments() {
+  db.collection('casenamelist').find({}).execute().then(docs => {
+    var html = docs.map(c => '<div>' + c.comment + '</div>').join('');
+    document.getElementById('casenamelist').innerHTML = html;
+  });
+}
 
 module.exports = router;
 
